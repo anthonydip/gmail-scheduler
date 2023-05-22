de663d134a262938627cb5184b230b508f596a1b
Anthony Dip <anthonydip2001@gmail.com>
2023-05-21 20:24:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+
+// Load index.html into new BrowserWindow instance
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('index.html');
+};
+
+// Open window when Electron app module is ready
+app.whenReady().then(() => {
+    createWindow();
+
+    // Open a window if none are open (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+});
+
+// Exiting all windows will quit application entirely (Windows + Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
